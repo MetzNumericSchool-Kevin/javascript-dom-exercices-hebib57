fb626e2ee9e228fd17f5dfd42adfae4bef6b24af
hebib57 <d-del57000@hotmail.fr>
2025-02-07 16:23:17 +0100

    Des informations manquent !

--- exercices.js
+++ exercices.js
@@ -4,3 +4,14 @@ const titre_boutique = document.querySelector("h1");
 const texte = titre_boutique.textContent;
 
 console.log(texte);
+
+//Des informations manquent !
+
+const description_boutique = document.querySelector("#description_boutique");
+
+const paragraphe = document.createElement("p");
+
+paragraphe.textContent =
+  "Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.";
+
+description_boutique.appendChild(paragraphe);
